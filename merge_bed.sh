69687eb1ea280c53fe557a5cb9e40a950b858c62
Kenichi Chiba <kchiba@hgc.jp>
2016-05-13 15:54:07 +0900

    modified the shell script that it can run with bedtools-2.24.0

--- merge_bed.sh
+++ merge_bed.sh
@@ -2,20 +2,20 @@
 
 PATH_TO_BEDTOOLS=$1
 
-${PATH_TO_BEDTOOLS}/mergeBed -i refGene.coding.exon.tmp.bed   -nms | ${PATH_TO_BEDTOOLS}/sortBed -i stdin > refGene.coding.exon.bed
+${PATH_TO_BEDTOOLS}/sortBed -i refGene.coding.exon.tmp.bed   | ${PATH_TO_BEDTOOLS}/mergeBed -i stdin -c 4 -o collapse > refGene.coding.exon.bed
 if [ ${PIPESTATUS[0]} -gt 0 -a ${PIPESTATUS[1]} -gt 0 ]; then
-  exit 1
+    exit 1
 fi
-${PATH_TO_BEDTOOLS}/mergeBed -i refGene.coding.intron.tmp.bed -nms | ${PATH_TO_BEDTOOLS}/sortBed -i stdin > refGene.coding.intron.bed
+${PATH_TO_BEDTOOLS}/sortBed -i refGene.coding.intron.tmp.bed  | ${PATH_TO_BEDTOOLS}/mergeBed -i stdin -c 4 -o collapse > refGene.coding.intron.bed
 if [ ${PIPESTATUS[0]} -gt 0 -a ${PIPESTATUS[1]} -gt 0 ]; then
-  exit 1
+    exit 1
 fi
-${PATH_TO_BEDTOOLS}/mergeBed -i refGene.coding.5putr.tmp.bed  -nms | ${PATH_TO_BEDTOOLS}/sortBed -i stdin > refGene.coding.5putr.bed
+${PATH_TO_BEDTOOLS}/sortBed -i refGene.coding.5putr.tmp.bed   | ${PATH_TO_BEDTOOLS}/mergeBed -i stdin -c 4 -o collapse > refGene.coding.5putr.bed
 if [ ${PIPESTATUS[0]} -gt 0 -a ${PIPESTATUS[1]} -gt 0 ]; then
-  exit 1
+    exit 1
 fi
-${PATH_TO_BEDTOOLS}/mergeBed -i refGene.coding.3putr.tmp.bed  -nms | ${PATH_TO_BEDTOOLS}/sortBed -i stdin > refGene.coding.3putr.bed
+${PATH_TO_BEDTOOLS}/sortBed -i refGene.coding.3putr.tmp.bed   | ${PATH_TO_BEDTOOLS}/mergeBed -i stdin -c 4 -o collapse > refGene.coding.3putr.bed
 if [ ${PIPESTATUS[0]} -gt 0 -a ${PIPESTATUS[1]} -gt 0 ]; then
-  exit 1
+    exit 1
 fi
 
